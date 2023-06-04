543a3f6ffbdc38a53d925c243b1de5fe53d3b8c6
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-06-04 22:59:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,24 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file_name(filename);
+  std::string first_line;
+  std::string text;
+  std::string addSymb;
+  while (std::getline(file_name, first_line)) {
+    text = text + first_line + " ";
+  }
+  for (auto a : text) {
+    if (isalpha(a)) {
+      a = tolower(a);
+      addSymb += a;
+    }
+    else {
+      tree.addElem(addSymb);
+      addSymb = "";
+    }
+  }
+  file_name.close();
+  return tree;
 }

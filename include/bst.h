d8c9d40954c60a8d81b5edab3667c0482fb8d9db
Ermokhin23iluxa <126194668+Ermokhin23iluxa@users.noreply.github.com>
2023-06-04 23:43:08 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,28 +4,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstring>
 #include <cctype>
 #include <algorithm>
 
 struct Node {
-  std::string first_word;
+  std::string word;
   int count;
   Node* left;
   Node* right;
-  explicit Node(const std::string& word) : first_word(word), count(1), left(nullptr), right(nullptr) {}
+  explicit Node(const std::string& word) : word(word), count(1), left(nullptr), right(nullptr) {}
 };
 template<typename T>
 class BST {
  private:
-  Node * root;
-  Node* addNode(Node* root, const std::string& word) {
+  Node* root;
+  Node* insertNode(Node* root, const std::string& word) {
     if (root == nullptr) {
       return new Node(word);
-    } if (word < root->word) {
-      root->left = addNode(root->left, word);
+    }
+    if (word < root->word) {
+      root->left = insertNode(root->left, word);
     } else if (word > root->word) {
-      root->right = addNode(root->right, word);
+      root->right = insertNode(root->right, word);
     } else {
       root->count++;
     }
@@ -41,22 +41,22 @@ class BST {
       return searchNode(root->right, word);
     }
   }
-  int get_depth(Node* root) {
+  int getDepth(Node* root) {
     if (root == nullptr) {
       return 0;
     }
-    int left_depth = get_depth(root->left);
-    int right_depth = get_depth(root->right);
-    return std::max(left_depth, right_depth) + 1;
+    int leftDepth = getDepth(root->left);
+    int rightDepth = getDepth(root->right);
+    return std::max(leftDepth, rightDepth) + 1;
   }
 
  public:
   BST() : root(nullptr) {}
-  void addElem(const std::string& word) {
-    root = addNode(root, word);
+  void addElement(const std::string& word) {
+    root = insertNode(root, word);
   }
   int depth() {
-    return get_depth(root) - 1;
+    return getDepth(root) - 1;
   }
   int search(const std::string& word) {
     Node* node = searchNode(root, word);
